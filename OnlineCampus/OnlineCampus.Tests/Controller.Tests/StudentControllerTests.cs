99f32241fc8a9c14986a443b5bf4d7022364129f
Thato <t.chitja369@gmail.com>
2024-06-04 13:04:09 +0200

    Add more tests for the Get Edit action

--- OnlineCampus/OnlineCampus.Tests/Controller.Tests/StudentControllerTests.cs
+++ OnlineCampus/OnlineCampus.Tests/Controller.Tests/StudentControllerTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using OnlineCampus.Controllers;
 using OnlineCampus.Interfaces;
@@ -22,6 +23,25 @@ namespace OnlineCampus.Tests.Controller.Tests
             // Assert
             var viewResult = Assert.IsType<ViewResult>(result);  // Ensures the result is a ViewResult
             Assert.Null(viewResult.ViewName);    // Ensures the view name is null, meaning it uses the default view.
+        }
+
+        [Fact]
+        public async Task Edit_StudentNotFound_SetsViewBagMessageAndReturnsView()
+        {
+            // Arrange
+            var studentId = Guid.NewGuid();
+            var mockRepository = new Mock<IStudentRepository>();
+            mockRepository.Setup(repo => repo.GetStudentByIdAsync(studentId))
+                .ReturnsAsync((Student)null);
+            var controller = new StudentController(mockRepository.Object);
+
+            // Act
+            var result = await controller.Edit(studentId);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Null(viewResult.Model);
+            Assert.Equal("The Student has not been found.", controller.ViewBag.Message);
         }
 
         [Fact]
@@ -51,6 +71,26 @@ namespace OnlineCampus.Tests.Controller.Tests
             Assert.Equal(student.FirstName, model.FirstName);
             Assert.Equal(student.LastName, model.LastName);
             Assert.Equal(student.RowVersion, model.RowVersion);
+        }
+
+        [Fact]
+        public async Task Edit_DbUpdateException_SetsModelErrorAndReturnsView()
+        {
+            // Arrange
+            var studentId = Guid.NewGuid();
+            var mockRepository = new Mock<IStudentRepository>();
+            mockRepository.Setup(repo => repo.GetStudentByIdAsync(studentId))
+                .ThrowsAsync(new DbUpdateException("Test exception", new Exception("Inner exception")));
+            var controller = new StudentController(mockRepository.Object);
+
+            // Act
+            var result = await controller.Edit(studentId);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Null(viewResult.Model); // ensure no model is returned
+            Assert.Equal("An error occurred while retrieving data from the database.", controller.ViewBag.Message); // Ensure ViewBag.Message is set
+            Assert.True(controller.ModelState.ContainsKey("")); // Ensure a model error is added
         }
 
         [Fact]
